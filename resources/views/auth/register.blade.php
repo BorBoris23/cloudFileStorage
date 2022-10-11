bcfdd8a6095cd87e85eee665cde96eb892975821
BorBoris23 <zonovboris@yandex.u>
2022-10-11 16:14:12 +0300

    fix-project name vertical alignment in the header and remove test form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            логотип
         </x-slot>
 
         <form method="POST" action="{{ route('register') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.header')
 
     @if(isset($authUser->name))
-        @include('dashboard')
+
     @endif
 
 @endsection
